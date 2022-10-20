ac662b2ef279ccd764482f9e7b63c8abb23df554
thomasj <jasminsoye.thomas@saintandrews.net>
2022-10-20 12:27:10 -0400

    ..

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         ShuntingYard blah = new ShuntingYard();
-        blah.infixToPostFix("5+1+3");
+        blah.evaluatePostFix("5+1+3");
     }
 }
--- src/com/company/ShuntingYard.java
+++ src/com/company/ShuntingYard.java
@@ -8,8 +8,8 @@ public class ShuntingYard {
     Stack<String> operators = new Stack<>();
     Queue<Object> numbers = new LinkedList<>();
 
-   String operatorList = "+-*/";
-   String numberList = "0123456789";
+    String operatorList = "+-*/";
+    String numberList = "0123456789";
 
     public String infixToPostFix(String expression) {
 /*1 + 1 infix
@@ -27,77 +27,72 @@ public class ShuntingYard {
                       Pop operators from the stack onto the output queue.
                 Pop the left bracket from the stack and discard it
  While there are operators on the stack, pop them to the queue*/
-        for(int i = 0; i<expression.length();i++) {
+        for (int i = 0; i < expression.length(); i++) {
             if (numberList.contains(expression.substring(i, i + 1))) {
                 addNumberToQueue(expression.substring(i, i + 1));
             }
-                if (operatorList.contains(expression.substring(i, i + 1))) {
-                    addOperatorToStack(expression.substring(i, i + 1));
-                }
-                if (expression.substring(i, i + 1).equals("(")) {
-                    operators.push(expression.substring(i, i + 1));
-                }
-                if (expression.substring(i, i + 1).equals(")")) {
-                    while (!operators.peek().equals("(")) {
-                        numbers.add(operators.pop());
-                    }
-                    operators.pop();
-                }
-
-
+            if (operatorList.contains(expression.substring(i, i + 1))) {
+                addOperatorToStack(expression.substring(i, i + 1));
             }
+            if (expression.substring(i, i + 1).equals("(")) {
+                operators.push(expression.substring(i, i + 1));
+            }
+            if (expression.substring(i, i + 1).equals(")")) {
+                while (!operators.peek().equals("(")) {
+                    numbers.add(operators.pop());
+                }
+                operators.pop();
+            }
+
+
+        }
         while (operators.size() != 0) {
             String operatorFromStack = operators.pop();
             numbers.add(operatorFromStack);
         }
-            while (numbers.size() > 0) {
-                System.out.print(numbers.remove());
-            }
+        while (numbers.size() > 0) {
+            System.out.print(numbers.remove());
+        }
 
         return null;
     }
 
-    public void addOperatorToStack(String operator){
+    public void addOperatorToStack(String operator) {
         //PEMDAS
-        if(operators.size() > 0) {
-            while(operators.size()>0 && isSecondOperatorGreaterOrEqual(operator, operators.peek())){
-                if(operators.peek().equals(")"))
-                numbers.add(operators.pop());
+        if (operators.size() > 0) {
+            while (operators.size() > 0 && isSecondOperatorGreaterOrEqual(operator, operators.peek())) {
+                if (operators.peek().equals(")"))
+                    numbers.add(operators.pop());
             }
             operators.push(operator);
-        }
-        else{
+        } else {
             operators.push(operator);
         }
 
     }
 
-    public void addNumberToQueue(String number){
-            numbers.add(number);
+    public void addNumberToQueue(String number) {
+        numbers.add(number);
     }
 
-    public void addNumberToStack (String number){
+    public void addNumberToStack(String number) {
         operators.push(number);
     }
 
-    public boolean isSecondOperatorGreaterOrEqual(String firstOperator, String secondOperator){
-        if(secondOperator == null){
+    public boolean isSecondOperatorGreaterOrEqual(String firstOperator, String secondOperator) {
+        if (secondOperator == null) {
 
         }
-        if(firstOperator.equals("+")||firstOperator.equals("-")){
-            if(secondOperator.equals("*")|| secondOperator.equals("/")||secondOperator.equals("(")
-                    ||secondOperator.equals(")")||secondOperator.equals("+")||secondOperator.equals("-")){
+        if (firstOperator.equals("+") || firstOperator.equals("-")) {
+            if (secondOperator.equals("*") || secondOperator.equals("/") || secondOperator.equals("(")
+                    || secondOperator.equals(")") || secondOperator.equals("+") || secondOperator.equals("-")) {
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
-        }
-        else if(firstOperator.equals("*")||firstOperator.equals("/")){
-           return true;
-        }
-
-       else if(firstOperator.equals("(")){
+        } else if (firstOperator.equals("*") || firstOperator.equals("/")) {
+            return true;
+        } else if (firstOperator.equals("(")) {
             return false;
         }
         return false;
@@ -114,22 +109,22 @@ push this string into stack.
 At the end only one value remain in stack which is our infix expression.
  */
 
-        for(int i = 0; i<expression.length();i++){
-            System.out.println("Checking: " + expression.substring(i,i+1));
-             if (numberList.contains(expression.substring(i, i + 1))) {
+        for (int i = 0; i < expression.length(); i++) {
+            System.out.println("Checking: " + expression.substring(i, i + 1));
+            if (numberList.contains(expression.substring(i, i + 1))) {
                 addNumberToStack(expression.substring(i, i + 1));
-                expression = expression.substring(i+1);
-                 i--;
+                expression = expression.substring(i + 1);
+                i--;
             }
-             //51+3+
-            else if(operatorList.contains(expression.substring(i,i+1))){
+            //51+3+
+            else if (operatorList.contains(expression.substring(i, i + 1))) {
                 String x = operators.pop();
                 String y = operators.pop();
-                operators.push(expression.substring(i,i+1));
+                operators.push(expression.substring(i, i + 1));
                 String operator = operators.pop();
                 String newString = "(" + y + operator + x + ")";
                 operators.push(newString);
-                expression = expression.substring(i+1);
+                expression = expression.substring(i + 1);
                 operators.push(expression);
                 i--;
             }
@@ -141,19 +136,54 @@ At the end only one value remain in stack which is our infix expression.
     }
 
     public boolean isValidExpression(String expressionToCheck) {
-        for(int i = 0; i<expressionToCheck.length();i++){
-            if("(".contains(expressionToCheck.substring(i,i+1))){
+        for (int i = 0; i < expressionToCheck.length(); i++) {
+            //more than one pair of parenthesis?
+            if ("(".contains(expressionToCheck.substring(i, i + 1))) {
+                if (expressionToCheck.contains(")")) {
 
+                } else {
+                    return false;
+                }
             }
-            //no letters
-            //operatos toger 1++2
-            //correct number of (), if u have ( then u have )
+            if (expressionToCheck.contains(operatorList) != true && expressionToCheck.contains(numberList) != true) {
+                return false;
+            }
+            if (expressionToCheck.substring(i, i + 1).contains(operatorList)) {
+                if (expressionToCheck.substring(i + 1, i + 2).contains(operatorList)) {
+                    return false;
+                } else {
+
+                }
+            }
         }
-        return false;
+        //no letters
+        //operatos toger 1++2
+        //correct number of (), if u have ( then u have )
+        return true;
     }
 
     public double evaluatePostFix(String expression) {
         //solve the equation
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.substring(i, i + 1).contains(numberList)) {
+                i = Integer.valueOf(i);
+            }
+            if (expression.substring(i, i + 1).contains(operatorList)) {
+                if (expression.substring(i, i + 1).contains("+")) {
+                    sum += (i+1);
+                }
+                if (expression.substring(i, i + 1).contains("-")) {
+                    sum -= (i+1);
+                }
+                if (expression.substring(i, i + 1).contains("*")) {
+                    sum *= (i+1);
+                }
+                if (expression.substring(i, i + 1).contains("/")) {
+                    sum /= (i+1);
+                }
+            }
+        }
+        return sum;
     }
 }
